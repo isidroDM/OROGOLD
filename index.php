b9e3cb7478bf51bf445c4cbf0d4b658b7f7fef00
isidroDM <isidro.biker@gmail.com>
2020-05-04 15:53:42 -0500

    Merge commit '29ef361427dc76ceeacd87f20708dee1c07200b1' into isidroBranch
    Conflicts:
    	js/datagraphinvestor.js
    	js/datagraphinvestoradmin.js
    	js/datagraphinvestormonth.js
    	js/datagraphinvestormonthadmin.js
    	js/datagraphinvestortoday.js
    	js/datagraphinvestortodayadmin.js
    	js/datagraphinvestoryear.js
    	js/datagraphinvestoryearadmin.js
    	nbproject/private/private.xml

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>hola</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
